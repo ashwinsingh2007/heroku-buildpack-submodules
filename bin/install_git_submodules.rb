277aa06db41db3c6aac144ecef8be4b89b188933
Ashwin Singh <legend.ashwini07@gmail.com>
2019-04-15 13:38:22 +0530

    adding ssh key

--- bin/install_git_submodules.rb
+++ bin/install_git_submodules.rb
@@ -8,6 +8,12 @@ config = ParseConfig.new("#{ENV['BUILD_DIR']}/.gitmodules")
 config.get_params.each do |param|
   next unless param.match(/^submodule/)
   c = config[param]
+  # https://d0c7d529cec1c5693d13b3574f366b176c0cbddb@github.com/ashwinsingh2007/herokuchild.git
+  url = c[url];
+  url = url.split('://', 1);
+  puts url
+  url = ` #{url[0]}d0c7d529cec1c5693d13b3574f366b176c0cbddb#{url[1]}`
+  puts url
 
   puts "-----> Installing submodule #{c["path"]} #{c["branch"]}"
   branch_flag = c["branch"] ? "-b #{c['branch']}" : ""
